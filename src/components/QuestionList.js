37f5d7595f01510661d6e7b797cdf04f178429e5
andisiambuku <roselandambuku@gmail.com>
2022-10-03 12:39:44 +0300

    Done

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,58 @@
-import React from "react";
+import { method } from "lodash";
+import React, { useState,useEffect }  from "react";
+import QuestionItem from "./QuestionItem";
+
+
+
 
 function QuestionList() {
+  const [question,setQuestion] = useState([]);
+
+  useEffect(() =>{
+    fetch("http://localhost:4000/questions")
+      .then((r)=>r.json())
+      .then((question)=>{
+        setQuestion(question);
+      } );
+    },[]);
+
+    function handleDelete(id){
+      fetch(`http://localhost:4000/questions/${id}`,{
+        method:"DELETE",
+      }
+      )
+      .then((r) => r.json())
+      .then(()=>{
+        const newQuestion = question.filter((q)=> q.id !==id);
+        setQuestion(newQuestion);
+      });
+    }
+    function handleChangedAnswer(id, correctIndex){
+      fetch(`http://localhost:4000/questions/${id}`,{
+        method:"PATCH",
+        headers:{
+          "Content-Type":"application/json",
+
+        },
+        body: JSON.stringify({ correctIndex }),
+      })
+      .then((r)=>r.json())
+      .then((newQuestions)=>{
+        const newQuestion = question.map((q)=>{
+          if (q.id === newQuestions.id) return newQuestions;
+          return q;
+        });
+        setQuestion(newQuestion);
+      });
+    }
+    const items = question.map((q)=>(
+      <QuestionItem key={q.id} question={q} onDelete={handleDelete} onChangeAnswer={handleChangedAnswer} />
+    ));
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{items}</ul>
     </section>
   );
 }
